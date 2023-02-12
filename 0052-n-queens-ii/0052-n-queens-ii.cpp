5766310391715faf829875df30045235e0da4732
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-02-13 00:18:18 +0530

    Time: 4 ms (75.14%), Space: 6.7 MB (29.31%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    vector<vector<string>> ans;
+    
+    bool is_valid(vector<string> &board,int row,int col){
+        for(int i=row;i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        
+        for(int i=row,j=col; i>=0 && j>=0 ; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        
+        for(int i=row,j=col ; i>=0 && j<board.size() ; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    void dfs(vector<string> &board,int row){
+        if(row == board.size()){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int col=0;col<board.size();col++){
+            if(is_valid(board,row,col)){
+                board[row][col] = 'Q';
+                dfs(board,row+1);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+         if(n <= 0){
+            return 0;
+        }
+        
+        vector<string> board(n,string(n,'.'));
+        
+        dfs(board,0);
+        
+        return ans.size();
+    }
+};
