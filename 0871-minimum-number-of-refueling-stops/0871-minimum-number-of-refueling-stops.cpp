91a38ddfa798b6d18b8d5baa6ad0fddbfb6e8ecb
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-02-16 21:30:06 +0530

    Time: 31 ms (57.14%), Space: 16.3 MB (57.85%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -8,7 +8,21 @@ public:
             return 0;
         }
         
-        if(n != 0 && startFuel >= stations[0][0]){            
+        if(n != 0 && startFuel >= stations[0][0]){
+            long long checkFuel = startFuel;
+            int fuelings = 0;
+            
+            for(int i=0;i<stations.size();i++){
+                if(checkFuel < stations[i][0]){
+                    return -1;
+                }
+                checkFuel += stations[i][1];
+            }
+            
+            if(target > checkFuel){
+                return -1;
+            }
+            
             int index = 0;
             int result;
             priority_queue<int> pq;
@@ -16,10 +30,6 @@ public:
             for(result = 0;startFuel<target;result++){
                 while(index<n && startFuel>=stations[index][0]){
                     pq.push(stations[index++][1]);
-                }
-                
-                if(pq.empty()){
-                    return -1;
                 }
                 
                 startFuel += pq.top();
