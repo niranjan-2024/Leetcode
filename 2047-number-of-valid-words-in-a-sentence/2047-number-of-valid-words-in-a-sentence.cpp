d1e83818501acad6e00f761157eb15a797d31d51
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-02-18 15:08:31 +0530

    Time: 27 ms (9.64%), Space: 9.8 MB (8.84%) - LeetHub

---
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    bool isValid(string s){
+        int n = s.length();
+        int hyphens = 0;
+        
+        for(int i=0;i<n;i++){
+            if(isalpha(s[i])){
+                continue;
+            }
+            if(isdigit(s[i])){
+                return false;
+            }
+            if((s[i] == '.' || s[i] == '!' || s[i] == ',')&&(i != n-1)){
+                return false;
+            }
+            if(s[i] == '-'){
+                if(hyphens || i == 0 || i == n-1 || !isalpha(s[i-1]) || !isalpha(s[i+1])){
+                    return false;
+                }
+                hyphens++;
+            }
+        }
+        
+        return true;
+    }
+    
+    int countValidWords(string sentence) {
+        vector<string> words;
+        
+        istringstream ss(sentence);
+        string curr;
+        
+        while(ss >> curr){
+            words.push_back(curr);
+        }
+        
+        for(auto s:words){
+            cout<<s<<endl;
+        }
+        
+        int ans = 0;
+        
+        for(string s:words){
+            if(isValid(s)){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
