6c45213b01f8314b68deeee933d4eaf862aa3458
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-06 12:17:54 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.55%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num == 1) return true;
+        int left = 0;
+        int right = num;
+        long long mid;
+        
+        while(left<right){
+            mid = left + (right-left)/2;
+            
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid<num){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        
+        return false;
+    }
+};
