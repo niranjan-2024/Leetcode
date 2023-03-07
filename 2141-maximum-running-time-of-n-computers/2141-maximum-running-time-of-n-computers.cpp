8de8d4bdc8b51ab29b445586aceb6b5002e4ba15
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-07 13:14:28 +0530

    Time: 155 ms (89.70%), Space: 55.9 MB (23.03%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    bool isPossible(long long int n,vector<int>& batteries,long long int avg){
+        long long int duration = 0;
+        for(int ele : batteries){
+            if(ele<avg){
+                duration += ele;
+            }
+            else{
+                duration += avg;
+            }
+        }
+        
+        return duration >= avg*n;
+    }
+    
+    long long maxRunTime(int n, vector<int>& batteries) {
+        
+        
+        long long int start = 0;
+        long long int end = 0;
+        
+        for(int ele:batteries){
+            end+=ele;
+        }
+        
+        long long avg;
+        long long ans = 0;
+        
+        while(start<=end){
+            avg = start + (end-start)/2;
+            if(isPossible(n,batteries,avg)){
+                ans = avg;
+                start = avg+1;
+            }
+            else{
+                end = avg-1;
+            }
+        }
+        
+        return ans;
+    }
+};
