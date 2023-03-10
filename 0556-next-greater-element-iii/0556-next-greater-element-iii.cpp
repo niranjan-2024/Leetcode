27bb535a4022ef173778ff710da1807e0c03a9f5
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-10 18:58:01 +0530

    Time: 3 ms (42.94%), Space: 6 MB (31.94%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,8 +1,34 @@
 class Solution {
 public:
+    
+    bool nextPer(string& nums){
+        if(nums.empty()){
+            return false;
+        }
+        int i = nums.size()-1;
+        while(i>=1 && nums[i]<=nums[i-1]){
+            i--;
+        }
+        if(i == 0){
+            return false;
+        }
+        
+        int j = nums.size()-1;
+        while(nums[j] <= nums[i-1]){
+            j--;
+        }
+        
+        swap(nums[i-1],nums[j]);
+        
+        reverse(nums.begin()+i,nums.end());
+        return true;
+    }
+    
     int nextGreaterElement(int n) {
         string nums = to_string(n);
-        next_permutation(nums.begin(),nums.end());
+        if(!nextPer(nums)){
+            return -1;
+        }
         auto ans = stoll(nums);
         return (ans>INT_MAX || ans<=n) ? -1 : ans;
     }
