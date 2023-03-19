041b2701435d77e096c3f7c7a55678263ad53583
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-19 10:25:31 +0530

    Time: 19 ms (66.67%), Space: 28.8 MB (33.33%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
     bool checkValidGrid(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m = grid[0].size();
         
         queue<pair<int, int>>q;
         q.push({0, 0});
@@ -21,7 +20,7 @@ public:
             for(int i=0; i<8; i++)
             {
                 int nx = x+dr[i], ny = y+dc[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==k+1)
+                if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==k+1)
                 {   x = nx,y = ny;
                     check = 1;
                     break;
@@ -34,6 +33,6 @@ public:
             
             k++;
         }
-        return k==(n*m) ? 1 : 0;
+        return k==(n*n) ? 1 : 0;
     }
 };
