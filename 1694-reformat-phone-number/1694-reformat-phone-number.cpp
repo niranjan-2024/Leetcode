c0e93762678b3d935fa05d5dc77eda3f541e2fc0
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-20 20:33:09 +0530

    Time: 4 ms (41.75%), Space: 6 MB (95.15%) - LeetHub

---
+++ 1694-reformat-phone-number/1694-reformat-phone-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string reformatNumber(string number) {
+        string temp;
+        string ans;
+        
+        int n = number.size();
+        
+        for(auto c:number){
+            if(isdigit(c)){
+                temp += c;
+            }
+        }
+        
+        int len = temp.size();
+        
+        while(len>0){
+            if(len == 3 || len > 4){
+                ans += temp.substr(0,3);
+                temp.erase(0,3);
+                len = len-3;
+                ans += "-";
+            }
+            else if(len == 4 || len == 2){
+                ans += temp.substr(0,2);
+                temp.erase(0,2);
+                ans += "-";
+                len = len-2;
+            }
+        }
+        
+        ans.pop_back();
+        return ans;
+    }
+};
