d03fa657da1f890484fd26383559acd47fa65a6c
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-03-22 15:27:10 +0530

    Time: 490 ms (69.39%), Space: 126.3 MB (70.31%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,72 @@
+class DisjointSet {
+    public:
+    vector<int> rank, parent, size; 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        DisjointSet ds(n+1);
+        
+        for(auto road:roads){
+            ds.unionBySize(road[0],road[1]);
+        }
+        
+        int mini = INT_MAX;
+        
+        int firstParent = ds.findUPar(1);
+        
+        for(auto road:roads){
+            if(firstParent == ds.findUPar(road[0])){
+                mini = min(mini,road[2]);
+            }
+        }
+        
+        return mini;
+    }
+};
