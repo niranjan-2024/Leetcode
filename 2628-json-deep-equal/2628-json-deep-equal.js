6688b9674a4a642d9aed76427beac2d4a4337d83
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-04-14 12:02:17 +0530

    Time: 79 ms (51.72%), Space: 47.3 MB (37.93%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -4,5 +4,24 @@
  * @return {boolean}
  */
 var areDeeplyEqual = function(o1, o2) {
-    return _.isEqual(o1,o2);
+    if(o1 === o2){
+        return true;
+    }
+    
+    if(typeof o1 !== 'object' || o1 === null || typeof o2 !== 'object' || o2 === null){
+        return false;
+    }
+    
+    if(Array.isArray(o1) !== Array.isArray(o2)){
+        return false;
+    }
+    
+    const k1 = Object.keys(o1);
+    const k2 = Object.keys(o2);
+    
+    if(k1.length !== k2.length){
+        return false;
+    }
+    
+    return k1.every(key => k2.includes(key) && areDeeplyEqual(o1[key],o2[key]));
 };
