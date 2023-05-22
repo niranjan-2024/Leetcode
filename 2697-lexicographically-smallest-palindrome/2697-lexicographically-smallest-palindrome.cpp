13ea5cbeaa3d386e672614eb8976e4a5cd2d76a4
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-05-22 17:30:57 +0530

    Time: 49 ms (30.77%), Space: 15 MB (61.54%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        for(int i=0;i<s.size()/2+1;i++){
+            s[i] = s[s.size()-i-1] = min(s[i],s[s.size()-i-1]);
+        }
+        
+        return s;
+    }
+};
