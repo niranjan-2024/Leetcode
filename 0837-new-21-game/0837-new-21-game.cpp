436a23b80f699a01bf9615ef53cab250fade40e5
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-05-25 13:27:49 +0530

    Time: 5 ms (58.87%), Space: 9.1 MB (85.48%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k == 0){
+            return 1.0;
+        }
+        if(n >= k-1+maxPts){
+            return 1.0;
+        }
+        
+        vector<double> dp(n+1,0.0);
+        
+        double probability = 0.0;
+        double windowSum = 1.0;
+        
+        dp[0] = 1.0;
+        
+        for(int i=1;i<=n;i++){
+            dp[i] = windowSum/maxPts;
+            
+            if(i<k){
+                windowSum += dp[i];
+            }
+            else{
+                probability += dp[i];
+            }
+            
+            if(i>=maxPts){
+                windowSum -= dp[i-maxPts];
+            }
+        }
+        
+        return probability;
+    }
+};
