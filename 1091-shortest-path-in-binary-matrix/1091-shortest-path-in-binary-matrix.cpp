0df11285c21c804ac54e092a7f4356e5dd18c9c7
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-06-01 16:19:05 +0530

    Time: 86 ms (44.09%), Space: 21.7 MB (53.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
         int n = grid.size();
         int m = grid[0].size();
         
         pair<int,int> source = {0,0};
         pair<int,int> destination = {n-1,m-1};
         
-        
-        
         if(grid[0][0] != 0){
             return -1;
         }
         
-        if(source.first==destination.first && source.second==destination.second){
+        if(source.first ==  destination.first && source.second == destination.second){
             return 1;
         }
         
-        queue<pair<int, pair<int,int>>> q;
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        dist[source.first][source.second] = 0;
+        queue<pair<int,pair<int,int>>> q;
+        vector<vector<int>> distance(n,vector<int>(m,1e9));
+        
+        distance[source.first][destination.first] = 0;
         q.push({0,{source.first,source.second}});
         
-        int dr[] = {-1,-1,0,1,1,1,0,-1};
-        int dc[] = {0,1,1,1,0,-1,-1,-1};
+        int drow[] = {-1,-1,-1,0,0,1,1,1};
+        int dcol[] = {-1,1,0,1,-1,-1,1,0};
         
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-            int distance = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
+            int currDistance = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
             
             for(int i=0;i<8;i++){
-                int newr = r + dr[i];
-                int newc = c + dc[i];
+                int newRow = row + drow[i];
+                int newCol = col + dcol[i];
                 
-                if(newr>=0 && newr<n && newc>=0 && newc<m 
-                && grid[newr][newc] == 0 && distance + 1 < dist[newr][newc] ){
-                    dist[newr][newc] = 1 + distance;
+                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==0 && currDistance+1 < distance[newRow][newCol]){
+                    distance[newRow][newCol] = 1+currDistance;
                     
-                    if(newr == destination.first && newc == destination.second){
-                        return distance + 1 + 1;
+                    if(newRow == destination.first && newCol == destination.second){
+                        return currDistance+1+1;
                     }
-                    q.push({1 + distance,{newr,newc}});
+                    
+                    q.push({1+currDistance,{newRow,newCol}});
                 }
             }
         }
