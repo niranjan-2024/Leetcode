2bac4a426f84217815b15bac503c60dd753e3bd8
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-06-03 02:38:06 +0530

    Time: 2141 ms (5.02%), Space: 574.8 MB (5.02%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     
-    int dfs(vector<vector<int>> &grid,int row,int col){
-        if(row>=0 && row<grid.size() && col>=0 && col<grid[0].size() && grid[row][col]==1){
-            grid[row][col] = 0;
-            return 1 + dfs(grid,row+1,col) + dfs(grid,row-1,col) + dfs(grid,row,col+1) + dfs(grid,row,col-1);
+    int bfs(vector<vector<int>> grid,int row,int col){
+        int area = 1;
+        
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        grid[row][col] = 0;
+        
+        int dx[] = {1,0,-1,0};
+        int dy[] = {0,-1,0,1};
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int newRow = row + dx[i];
+                int newCol = col + dy[i];
+                
+                if(newRow>=0 && newRow<grid.size() && newCol>=0 && newCol<grid[0].size() && grid[newRow][newCol] == 1){
+                    grid[newRow][newCol] = 0;
+                    area++;
+                    q.push({newRow,newCol});
+                }
+            }
         }
         
-        return 0;
+        return area;
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -16,7 +37,7 @@ public:
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j] == 1){
-                    max_area = max(max_area,dfs(grid,i,j));
+                    max_area = max(max_area,bfs(grid,i,j));
                 }
             }
         }
