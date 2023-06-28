dce0ff58a8428e1b2e6e75a92e460842648f3d1e
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-06-28 12:34:43 +0530

    Time: 191 ms (94.62%), Space: 66.5 MB (42.55%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int,double>>> adjList(n);
         
+        vector<vector<pair<int,double>>> adjList(n);
         for(int i=0;i<edges.size();i++){
             adjList[edges[i][0]].push_back({edges[i][1],succProb[i]});
             adjList[edges[i][1]].push_back({edges[i][0],succProb[i]});
@@ -10,11 +10,11 @@ public:
         
         vector<int> visited(n,0);
         
-        priority_queue<pair<double,int>> pq;
-        pq.push({(double)1.0,start});
-        
         vector<double> maxAns(n,(double)0.0);
         maxAns[start] = 1.0;
+        
+        priority_queue<pair<double,int>> pq;
+        pq.push({(double)1.0,start});
         
         while(!pq.empty()){
             auto it = pq.top();
@@ -26,7 +26,7 @@ public:
                 visited[node] = 1;
                 
                 for(auto &adjacentNode:adjList[node]){
-                    if(maxAns[adjacentNode.first]<adjacentNode.second*probability){
+                    if(maxAns[adjacentNode.first] < adjacentNode.second*probability){
                         maxAns[adjacentNode.first] = adjacentNode.second*probability;
                         pq.push({maxAns[adjacentNode.first],adjacentNode.first});
                     }
