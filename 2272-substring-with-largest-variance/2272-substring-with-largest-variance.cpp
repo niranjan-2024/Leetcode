0901a3717d3995be5f69a596aef788213ef46306
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-07-09 12:28:28 +0530

    Time: 238 ms (42.77%), Space: 6.9 MB (62.26%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int largestVariance(string s) {
-        int n = s.size();
         int ans = 0;
         
         for(char p='a';p<='z';p++){
@@ -15,21 +14,22 @@ public:
                 
                 bool canExtendPrevQ = false;
                 
-                for(auto c:s){
+                for(char c:s){
+                     
                     if(c == p){
                         pCount++;
                     }
                     if(c == q){
                         qCount++;
                     }
-                    
+
                     if(qCount > 0){
                         ans = max(ans,pCount-qCount);
                     }
                     else if(qCount == 0 && canExtendPrevQ){
                         ans = max(ans,pCount-1);
                     }
-                    
+
                     if(qCount > pCount){
                         qCount = pCount = 0;
                         canExtendPrevQ = true;
