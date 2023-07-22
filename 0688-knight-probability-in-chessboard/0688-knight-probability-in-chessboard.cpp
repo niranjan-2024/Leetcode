b38f06ec936c55f45b34758b501115768afcd5a6
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-07-22 15:11:03 +0530

    Time: 39 ms (26.80%), Space: 8.7 MB (59.98%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     
-    vector<vector<int>> directions = {{-2,-1},{-1,-2},{1,2},{2,1},{-1,2},{2,-1},{-2,1},{1,-2}};
-    
-    double solve(int n,int k,int r,int c,vector<vector<vector<double>>> &dp){
-        if(r<0 || r>n-1 || c<0 || c>n-1){
-            return 0;
-        }
-        if(k == 0){
-            return 1;
-        }
-        
-        if(dp[r][c][k] != 0){
-            return dp[r][c][k];
-        }
-        
-        double rate = 0;
-        for(int i=0;i<directions.size();i++){
-            rate += 0.125*solve(n,k-1,r+directions[i][0],c+directions[i][1],dp);
-        }
-        
-        return dp[r][c][k] = rate;
-    }
-    
     double knightProbability(int n, int k, int row, int column) {
         vector<vector<vector<double>>> dp(n,vector<vector<double>>(n,vector<double>(k+1,0)));
-        return solve(n,k,row,column,dp);
+        
+        vector<vector<int>> directions = {{-2,-1},{-1,-2},{1,2},{2,1},{-1,2},{2,-1},{-2,1},{1,-2}};
+        
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
+                dp[r][c][0] = 1;
+            }
+        }
+        
+        for(int K=1;K<=k;K++){
+            for(int r=0;r<n;r++){
+                for(int c=0;c<n;c++){
+                    
+                    double rate = 0;
+                    
+                    for(int i=0;i<directions.size();i++){
+                        
+                        int newR = r + directions[i][0];
+                        int newC = c + directions[i][1];
+                        
+                        if(newR>=0 && newR<n && newC>=0 && newC<n){
+                            rate += 0.125*dp[newR][newC][K-1];
+                        }
+                    }
+                    
+                    dp[r][c][K] = rate;
+                }
+            }
+        }
+        
+        return dp[row][column][k];
     }
 };
