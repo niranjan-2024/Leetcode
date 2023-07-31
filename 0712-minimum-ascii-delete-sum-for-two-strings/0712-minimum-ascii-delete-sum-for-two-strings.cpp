5145040c0fce0c3d0575cb7b16ce7a5fab346977
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-07-31 13:29:42 +0530

    Time: 19 ms (94.62%), Space: 6.5 MB (99.06%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -14,19 +14,21 @@ public:
             sum += s2[i];
         }
         
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        vector<int> curr(n2+1,0);
+        vector<int> next(n2+1,0);
         
         for(int i=n1-1;i>=0;i--){
             for(int j=n2-1;j>=0;j--){
                 if(s1[i] == s2[j]){
-                    dp[i][j] = s1[i] + dp[i+1][j+1];
+                    curr[j] = s1[i] + next[j+1];
                 }
                 else{
-                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                    curr[j] = max(next[j],curr[j+1]);
                 }
             }
+            next = curr;
         }
         
-        return sum - (2*dp[0][0]);
+        return sum - (2*next[0]);
     }
 };
