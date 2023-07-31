2c695b93cf1fd9676584c47380f441751029140f
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-07-31 13:26:48 +0530

    Time: 26 ms (88.95%), Space: 15.2 MB (68.25%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,27 +1,6 @@
 class Solution {
 public:
     
-    int LCS(string &s1,string &s2,int i,int j,vector<vector<int>> &dp){
-        if(i == s1.length() || j == s2.length()){
-            return 0;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int ans = 0;
-        
-        if(s1[i] == s2[j]){
-            ans = s1[i] + LCS(s1,s2,i+1,j+1,dp);
-        }
-        else{
-            ans = max(LCS(s1,s2,i+1,j,dp),LCS(s1,s2,i,j+1,dp));
-        }
-        
-        return dp[i][j] = ans;
-    }
-    
     int minimumDeleteSum(string s1, string s2) {
         int n1 = s1.length();
         int n2 = s2.length();
@@ -35,8 +14,19 @@ public:
             sum += s2[i];
         }
         
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
         
-        return sum - (2*(LCS(s1,s2,0,0,dp)));
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                if(s1[i] == s2[j]){
+                    dp[i][j] = s1[i] + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        
+        return sum - (2*dp[0][0]);
     }
 };
