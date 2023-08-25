1ce0add5859393605a0361fd2f9d1a36fc8569e8
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-08-25 23:23:40 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (41.69%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     
-    bool solve(string &s1,string &s2,string &s3,int l1,int l2,int l3){
-        if(l1<0 && l2<0 && l3<0){
+    unordered_map<string, bool> memo;
+
+    bool solve(string &s1, string &s2, string &s3, int l1, int l2, int l3) {
+        if (l1 < 0 && l2 < 0 && l3 < 0) {
             return true;
         }
-        
-        if(l1>=0 && s1[l1]==s3[l3] && l2>=0 && s2[l2]==s3[l3]){
-            return solve(s1,s2,s3,l1-1,l2,l3-1) || solve(s1,s2,s3,l1,l2-1,l3-1);
+
+        string key = to_string(l1) + "#" + to_string(l2) + "#" + to_string(l3);
+
+        if (memo.find(key) != memo.end()) {
+            return memo[key];
         }
-        else if(l1>=0 && s1[l1]==s3[l3]){
-            return solve(s1,s2,s3,l1-1,l2,l3-1);
+
+        bool result = false;
+
+        if (l1 >= 0 && s1[l1] == s3[l3] && l2 >= 0 && s2[l2] == s3[l3]) {
+            result = solve(s1, s2, s3, l1 - 1, l2, l3 - 1) || solve(s1, s2, s3, l1, l2 - 1, l3 - 1);
+        } else if (l1 >= 0 && s1[l1] == s3[l3]) {
+            result = solve(s1, s2, s3, l1 - 1, l2, l3 - 1);
+        } else if (l2 >= 0 && s2[l2] == s3[l3]) {
+            result = solve(s1, s2, s3, l1, l2 - 1, l3 - 1);
         }
-        else if(l2>=0 && s2[l2]==s3[l3]){
-            return solve(s1,s2,s3,l1,l2-1,l3-1);
-        }
-        else{
-            return false;
-        }
+
+        memo[key] = result;
+
+        return result;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
