1f60e6398100692bade3058f68b86cbb8b5bc218
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-08-25 12:53:18 +0530

    Time: 1317 ms (5.02%), Space: 6.1 MB (96.97%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    bool solve(string &s1,string &s2,string &s3,int l1,int l2,int l3){
+        if(l1<0 && l2<0 && l3<0){
+            return true;
+        }
+        
+        if(l1>=0 && s1[l1]==s3[l3] && l2>=0 && s2[l2]==s3[l3]){
+            return solve(s1,s2,s3,l1-1,l2,l3-1) || solve(s1,s2,s3,l1,l2-1,l3-1);
+        }
+        else if(l1>=0 && s1[l1]==s3[l3]){
+            return solve(s1,s2,s3,l1-1,l2,l3-1);
+        }
+        else if(l2>=0 && s2[l2]==s3[l3]){
+            return solve(s1,s2,s3,l1,l2-1,l3-1);
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int l3 = s3.length();
+        
+        if(l3 != l1+l2){
+            return false;
+        }
+        
+        return solve(s1,s2,s3,l1-1,l2-1,l3-1);
+    }
+};
