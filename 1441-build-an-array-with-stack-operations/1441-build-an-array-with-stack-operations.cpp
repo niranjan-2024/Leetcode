bd5226525e53d0e4d954b2dc742d63b73159628a
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-11-03 18:35:11 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (75.36%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -2,43 +2,16 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        stack<int> st;
+        int streamCurrent = 1;
         
-        vector<int> base = {1};
-        if(target == base){
-            return {"Push"};
-        }
-        
-        int m = target.size();
-        int j=1;
-        int i=1;
-        int k=0;
-        
-        while(i<=n && j<m){
-            while(!st.empty() && st.top() != target[k]){
-                st.pop();
+        for(int i=0;i<target.size();i++){
+            while(streamCurrent < target[i]){
+                ans.push_back("Push");
                 ans.push_back("Pop");
+                streamCurrent++;
             }
-            
-            st.push(i);
-            i++;
+            streamCurrent++;
             ans.push_back("Push");
-            
-            
-            if(!st.empty() && target[j] == st.top()){
-                j++;
-                k++;
-            }
-        }
-        
-        vector<int> num;
-        while(!st.empty()){
-            num.push_back(st.top());
-            st.pop();
-        }
-        reverse(num.begin(),num.end());
-        for(int i=0;i<num.size();i++){
-            cout<<num[i]<<" ";
         }
         
         return ans;
