c0ec2583414d0c03b292977910349bb6429fa113
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-11-07 18:58:47 +0530

    Time: 96 ms (92.53%), Space: 85 MB (60.80%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.cpp
@@ -5,7 +5,7 @@ public:
         vector<double> time_to_city(n);
         
         for (int i = 0; i < n; ++i) {
-            time_to_city[i] = static_cast<double>(dist[i]) / speed[i];
+            time_to_city[i] = (double)(dist[i]) / speed[i];
         }
         
         sort(time_to_city.begin(), time_to_city.end());
