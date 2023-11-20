7a2a4c342bb5347727b82433b402a879dffa88bc
niranjan-2024 <89894972+niranjan-2024@users.noreply.github.com>
2023-11-20 14:06:54 +0530

    Time: 200 ms (12.90%), Space: 102.4 MB (58.71%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,90 +1,40 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int metal = 0;
-        int glass = 0;
-        int paper = 0;
+    
+    int collectGarbage(vector<string>& garbage, vector<int>& travel, char type){
+        int tempPrev = 0;
+        int cost = 0;
         
+        for(int i=0;i<garbage.size();i++){
+            int temp = 0;
+            bool tempAdd = false;
+            
+            if(i>0){
+                temp = travel[i-1];
+            }
+            
+            for(int j=0;j<garbage[i].length();j++){
+                if(garbage[i][j] == type){
+                    cost += 1;
+                    tempAdd = true;
+                }
+            }
+            
+            if(tempAdd == true){
+                cost -= tempPrev;
+                cost += temp;
+                tempPrev = temp;
+            }
+        }
+        
+        return cost;
+    }
+    
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         for(int i=1;i<travel.size();i++){
             travel[i] += travel[i-1];
         }
         
-        int n = garbage.size();
-        
-        int tempMetalPrev = 0;
-        int tempPaperPrev = 0;
-        int tempGlassPrev = 0;
-        
-        for(int i=0;i<n;i++){
-            int temp = 0;
-            bool tempAdd = false;
-            
-            if(i>0){
-                temp = travel[i-1];
-            }
-            
-            for(int j=0;j<garbage[i].length();j++){
-                if(garbage[i][j] == 'M'){
-                    metal += 1;
-                    tempAdd = true;
-                }
-            }
-            
-            if(tempAdd == true){
-                metal -= tempMetalPrev;
-                metal += temp;
-                tempMetalPrev = temp;
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            int temp = 0;
-            bool tempAdd = false;
-            
-            if(i>0){
-                temp = travel[i-1];
-            }
-            
-            for(int j=0;j<garbage[i].length();j++){
-                if(garbage[i][j] == 'P'){
-                    paper += 1;
-                    tempAdd = true;
-                }
-            }
-            
-            if(tempAdd == true){
-                paper -= tempPaperPrev;
-                paper += temp;
-                tempPaperPrev = temp;
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            int temp = 0;
-            bool tempAdd = false;
-            
-            if(i>0){
-                temp = travel[i-1];
-            }
-            
-            for(int j=0;j<garbage[i].length();j++){
-                if(garbage[i][j] == 'G'){
-                    glass += 1;
-                    tempAdd = true;
-                }
-            }
-            
-            if(tempAdd == true){
-                glass -= tempGlassPrev;
-                glass += temp;
-                tempGlassPrev = temp;
-            }
-        }
-        
-        // cout<<"metal "<<metal<<endl;
-        // cout<<"paper "<<paper<<endl;
-        // cout<<"glass "<<glass<<endl;
-        
-        return (metal + paper + glass);
+        return collectGarbage(garbage,travel,'M') + collectGarbage(garbage,travel,'P') + collectGarbage(garbage,travel,'G');
     }
 };
